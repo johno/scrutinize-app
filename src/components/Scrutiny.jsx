06ed61c5a9236940d793aff7c45a33c276bd0c09
John Otander <johnotander@gmail.com>
2015-02-11 16:31:00 -0700

    Don't load a url right away

--- src/components/Scrutiny.jsx
+++ src/components/Scrutiny.jsx
@@ -3,26 +3,11 @@
 var React = require('react')
 var TopStats = require('./TopStats.jsx')
 var SizeStats = require('./SizeStats.jsx')
+var UrlForm = require('./UrlForm.jsx')
 
 module.exports = React.createClass({
-  loadScrutinyFromServer: function() {
-    $.ajax({
-      url: 'http://scrutinize.herokuapp.com/?url=' + url.url,
-      dataType: 'json',
-      success: function(data) {
-        this.setState({ data: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
   getInitialState: function() {
     return { data: { title: this.props.url, a11y: { failures: [] }, psi: {}, css: {}, domStats: {} } }
-  },
-
-  componentDidMount: function() {
-    this.loadScrutinyFromServer();
   },
 
   handleUrlSubmit: function(url) {
--- src/components/UrlForm.jsx
+++ src/components/UrlForm.jsx
@@ -14,7 +14,7 @@ module.exports = React.createClass({
   },
   render: function() {
     return (
-      <form className="urlForm" onSubmit={this.handleSubmit}
+      <form className="urlForm" onSubmit={this.handleSubmit}>
         <input type="text" name="url" ref="url" placeholder="Enter a url" className="field-light" />
         <button type="submit" className="button-blue ml1">
           Scrutinize
