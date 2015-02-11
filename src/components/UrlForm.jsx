96803f2c39d3a419e32afefa3f8007fd411e4518
John Otander <johnotander@gmail.com>
2015-02-11 14:41:00 -0700

    Add more form styling

--- src/components/UrlForm.jsx
+++ src/components/UrlForm.jsx
@@ -14,9 +14,11 @@ module.exports = React.createClass({
   },
   render: function() {
     return (
-      <form className="urlForm" onSubmit={this.handleSubmit}>
-        <input type='text' placeholder='Enter a url...' ref='url'/>
-        <input type='submit' value='Scrutinize Url' />
+      <form className="urlForm" onSubmit={this.handleSubmit}
+        <input type="text" name="url" ref="url" placeholder="Enter a url" className="field-light" />
+        <button type="submit" className="button-blue ml1">
+          Scrutinize
+        </button>
       </form>
     )
   }
